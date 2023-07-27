85ad1e35e2e981b46206a709c2e164178d8cc6ea
Mingyu Kim <119953993+gold9387@users.noreply.github.com>
2023-07-27 10:44:54 +0900

    1조 캠핑 예약 & 용품 판매 사이트 프로젝트 생성

---
+++ src/main/java/com/camping/site/CampingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.camping.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampingSiteApplication.class, args);
+	}
+
+}
